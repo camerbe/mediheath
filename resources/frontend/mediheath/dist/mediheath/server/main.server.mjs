243c8bd73d541d7ec4d84585dc7809d985560600
camerbe <tristanvellozzi@gmail.com>
2026-06-20 16:17:04 +0200

    remove git

--- resources/frontend/mediheath/dist/mediheath/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-FQ7WT5W7.mjs";import"./chunk-YMENXRVB.mjs";import"./chunk-NGK3KENG.mjs";import"./chunk-PT2C2EN2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
