5e062bce330ae6f1088aa75a43911714de6e0d55
camerbe <tristanvellozzi@gmail.com>
2026-06-24 00:38:50 +0200

    upd dist

--- resources/frontend/mediheath/dist/mediheath/server/main.server.mjs
+++ resources/frontend/mediheath/dist/mediheath/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NY4HZGAL.mjs";import"./chunk-YMENXRVB.mjs";import"./chunk-NGK3KENG.mjs";import"./chunk-PT2C2EN2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-QTFKBZEV.mjs";import"./chunk-YMENXRVB.mjs";import"./chunk-NGK3KENG.mjs";import"./chunk-PT2C2EN2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- resources/frontend/mediheath/src/environments/environment.production.ts
+++ resources/frontend/mediheath/src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: "https://api.medihealth-clinic.org",
+  baseUrl: "https://api.medihealth-clinic.org/api",
   serverUrl: "https://www.medihealth-clinic.org"
 
 };
