0e0d2886f81c052a7468f5c3138c056546855a8b
camerbe <tristanvellozzi@gmail.com>
2026-07-10 01:53:24 +0200

    rollback changes to previous version

--- resources/frontend/mediheath/dist/mediheath/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-BEA5K2NY.mjs";import"./chunk-HGJLLC5T.mjs";import"./chunk-NGK3KENG.mjs";import"./chunk-AAVIZABW.mjs";import"./chunk-BDV5NHRX.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
